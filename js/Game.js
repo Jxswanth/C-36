554103bf9e0315a9172c269b49bbd73ebc7d17c3
unknown <Narneni99@gmail.com>
2020-11-22 12:01:13 +0530

    added Player and distance

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,26 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hide();
+    textSize(30);
+    text("GAME STARTED",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers!==undefined){
+   var yPos =130;
+   for(var i in allPlayers){
+     if(i==="player"+player.index)
+     fill("red");
+     else
+     fill("black");
+     yPos+=20
+     textSize(15)
+     text(allPlayers[i].name+ ": "+allPlayers[i].distance,120,yPos)
+   }
+    }
+    if(keyIsDown(UP_ARROW)&&player.index!==null){
+      player.distance+=50
+      player.update()
+    }
+  }
 }
